3c16baba191451738a95ac276d08f90944f90b34
narcisse66 <dossanarcisse242@gmail.com>
2025-09-20 21:49:38 +0100

    Ma  mise à jour10

--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -3,7 +3,7 @@ import gallery_2 from "../../assets/gallery/gallery2.jpg";
 import gallery_3 from "../../assets/gallery/gallery3.jpg";
 import gallery_4 from "../../assets/gallery/gallery4.jpg";
 import gallery_5 from "../../assets/gallery/gallery5.jpg";
-import gallery_6 from "../../assets/gallery/gallery6.jpg";
+import gallery_6 from "../../assets/gallery/gallery6_2.jpg";
 import gallery_7 from "../../assets/gallery/gallery7.jpg";
 import gallery_8 from "../../assets/gallery/gallery8.jpg";
 
